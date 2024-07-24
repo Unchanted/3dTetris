722e3b21ab38717ab25a2fc3e070055cc5a0fed1
Unchanted <omkkdwivedi@gmail.com>
2024-07-24 20:30:49 +0530

    setup completion

--- src/cubev.js
+++ src/cubev.js
@@ -7,6 +7,23 @@ var sizeScale = 1;
 var objects = []
 var mainObject = null;
 var mainObjectIndex = 1;
+var cameraTheta = [1,1,1];
+var camera_theta_loc;
+var idle_rotation_vel = 1.0;
+const gravity_speed_init = 0.005;
+var gravity_speed = gravity_speed_init;
+var direction = 1;
+var move_scale =0.1
+var rotate_scale = 4;
+const directions = {
+	"RIGHT"	: [ 0,1],
+	"LEFT"	: [0,-1],
+	"UP"	: [ 1,1],
+	"DOWN" 	: [1,-1],
+//	"FRONT" : [2,1]
+
+}
+var program;
 
 
 class Object{
@@ -42,15 +59,7 @@ class Object{
 
 
 
-const directions = {
-	"RIGHT"	: [ 0,1],
-	"LEFT"	: [0,-1],
-	"UP"	: [ 1,1],
-	"DOWN" 	: [1,-1],
-//	"FRONT" : [2,1]
 
-}
-var program;
 function buffer(obj){
 	
     var iBuffer = gl.createBuffer();
@@ -89,7 +98,7 @@ window.onload = function init()
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
 
     gl.enable(gl.DEPTH_TEST);;
-	console.log("number of obj in scene: ",initObjects.length);
+	console.log("Sahnedeki obje sayısı: ",initObjects.length);
 	
 	
 	program = initShaders( gl, "vertex-shader", "fragment-shader" );
@@ -99,10 +108,108 @@ window.onload = function init()
 		objects.push(new Object(initObjects[i].vertices,initObjects[i].colors,initObjects[i].indices));	
 		buffer(objects[i]);
 	}
-
+	camera_theta_loc = gl.getUniformLocation(program, "camera");
 	mainObject = objects[mainObjectIndex];
 		
 	render();
 }
 
-var idle_rotation_vel = 1.0;
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	
+	for(var i=0;i<objects.length;i++){
+		
+		if(getBottom(objects[i].getVertices()) > -1)
+			move(objects[i],gravity_speed,directions.DOWN);
+		if(i==1)
+			console.log("bottom:" ,getBottom(objects[i].getVertices()));
+		gl.uniform3fv(objects[i].getThetaLoc(), objects[i].getTheta());
+		
+		buffer(objects[i]);
+		gl.drawElements(gl.TRIANGLES, objects[i].getIndices().length, gl.UNSIGNED_BYTE, 0);
+		
+	}
+
+    requestAnimFrame( render );
+}
+
+
+window.onkeydown = function(event) {
+	let key = String.fromCharCode(event.keyCode).toLowerCase();
+	switch(key){
+		
+		case '&':
+			rotate(mainObject,directions.UP);
+			break;
+		case '%':
+			rotate(mainObject,directions.LEFT);
+			break;
+		case '(':
+			rotate(mainObject,directions.DOWN);
+			break;
+		case '\'':
+			rotate(mainObject,directions.RIGHT);
+			break;
+			
+		case 'w':
+			move(mainObject,move_scale,directions.UP);
+			break;
+		case 'a':
+			move(mainObject,move_scale,directions.LEFT);
+			break;
+		case 's':
+			move(mainObject,move_scale,directions.DOWN);
+			break;
+		case 'd':
+			move(mainObject,move_scale,directions.RIGHT);
+			break;
+		case ' ':
+			gravity_speed = 0.01;
+			break;
+		case 'c':
+			cameraTheta[1] +=1;
+			gl.uniform3fv(camera_theta_loc, cameraTheta);
+			break;
+			
+	}
+}
+
+window.onkeyup = function(){
+	let key = String.fromCharCode(event.keyCode).toLowerCase();
+	
+	switch(key){
+		case ' ':
+			gravity_speed = gravity_speed_init;
+			break;
+		
+	}
+	
+}
+function rotate(object,dir_enum){
+	let index = 1 - dir_enum[0];
+	let direction = (2*index-1)*dir_enum[1];
+	object.changeTheta(index,object.getTheta()[index]+direction*rotate_scale); 
+}
+
+function move(object,move_scale,dir_enum){
+	
+	let index = dir_enum[0];
+	let direction = dir_enum[1];
+	let pay = 180 - Math.abs(object.getTheta()[1-index]);
+	let direction_rotation_fix = pay/Math.abs(pay) ;
+	let vertices = object.getVertices();
+	for(let i=0;i<vertices.length;i++)
+		vertices[i][index]+=direction*move_scale*direction_rotation_fix;
+	object.setVertices(vertices);
+}
+
+function getBottom(vertices){
+	let min = 1;
+	for(var i=0;i<vertices.length;i++)
+			if(vertices[i][1] < min)
+				min = vertices[i][1];
+	return min;
+}
